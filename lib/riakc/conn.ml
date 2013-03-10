343e484a715162c47fe02570f027c5675303a62f
orbitz <orbitz@gmail.com>
2013-03-10 23:16:44 +0100

    Rewrite to remove the variant in the response

--- lib/riakc/conn.ml
+++ lib/riakc/conn.ml
@@ -49,29 +49,37 @@ let rec read_str r pos s =
 let parse_length preamble =
   Deferred.return (Response.parse_length preamble)
 
+let parse_mc payload =
+  Deferred.return (Response.parse_mc payload)
+
 let read_payload r preamble =
   let open Deferred.Result.Monad_infix in
   parse_length preamble >>= fun resp_len ->
   let payload = String.create resp_len in
   read_str r 0 payload
 
-let rec read_response r =
+let rec read_response mc r f =
   let open Deferred.Result.Monad_infix in
   let preamble = String.create 4 in
   read_str r 0 preamble   >>= fun _ ->
-  read_payload r preamble >>= fun payload ->
-  Deferred.return (Response.of_string payload) >>= function
-    | Response.More resp ->
-      read_response r >>= fun more ->
-      Deferred.return (Ok (resp::more))
-    | Response.Done resp ->
-      Deferred.return (Ok [resp])
+  read_payload r preamble >>= fun mc_payload ->
+  parse_mc mc_payload     >>= function
+    | (p_mc, payload) when p_mc = mc -> begin
+      Deferred.return (f payload) >>= function
+	| Response.More resp ->
+	  read_response mc r f >>= fun more ->
+	  Deferred.return (Ok (resp::more))
+	| Response.Done resp ->
+	  Deferred.return (Ok [resp])
+    end
+    | _ ->
+      Deferred.return (Error `Bad_payload)
 
-let do_request t f =
+let do_request t mc g f =
   let open Deferred.Result.Monad_infix in
-  Deferred.return (f ())    >>= fun request ->
+  Deferred.return (g ())    >>= fun request ->
   Writer.write t.w request;
-  read_response t.r
+  read_response mc t.r f
 
 let connect ~host ~port =
   let connect () =
@@ -88,8 +96,13 @@ let close t =
   Deferred.return (Ok ())
 
 let ping t =
-  do_request t Request.ping >>| function
-    | Ok [Response.Ping] ->
+  do_request
+    t
+    0x02
+    Request.ping
+    Response.ping
+  >>| function
+    | Ok [()] ->
       Ok ()
     | Ok _ ->
       Error `Wrong_type
@@ -97,8 +110,13 @@ let ping t =
       Error err
 
 let client_id t =
-  do_request t Request.client_id >>| function
-    | Ok [Response.Client_id client_id] ->
+  do_request
+    t
+    0x04
+    Request.client_id
+    Response.client_id
+  >>| function
+    | Ok [client_id] ->
       Ok client_id
     | Ok _ ->
       Error `Wrong_type
@@ -106,8 +124,13 @@ let client_id t =
       Error err
 
 let server_info t =
-  do_request t Request.server_info >>| function
-    | Ok [Response.Server_info (node, version)] ->
+  do_request
+    t
+    0x07
+    Request.server_info
+    Response.server_info
+  >>| function
+    | Ok [(node, version)] ->
       Ok (node, version)
     | Ok _ ->
       Error `Wrong_type
@@ -115,8 +138,13 @@ let server_info t =
       Error err
 
 let list_buckets t =
-  do_request t Request.list_buckets >>| function
-    | Ok [Response.Buckets buckets] ->
+  do_request
+    t
+    0x10
+    Request.list_buckets
+    Response.list_buckets
+  >>| function
+    | Ok [buckets] ->
       Ok buckets
     | Ok _ ->
       Error `Wrong_type
@@ -124,16 +152,14 @@ let list_buckets t =
       Error err
 
 let list_keys t bucket =
-  do_request t (Request.list_keys bucket) >>| function
-    | Ok keys -> begin
-      Ok (List.concat_map
-	    ~f:(function
-	      | Response.Keys keys ->
-		keys
-	      | _ ->
-		failwith "list_keys_bad_response")
-	    keys)
-    end
+  do_request
+    t
+    0x12
+    (Request.list_keys bucket)
+    Response.list_keys
+  >>| function
+    | Ok keys ->
+      Ok (List.concat keys)
     | Error err ->
       Error err
 
--- lib/riakc/response.ml
+++ lib/riakc/response.ml
@@ -6,16 +6,9 @@ open Core.Std
 
 module P = Protobuf.Parser
 
-type t =
-  | Ping
-  | Client_id of string
-  | Server_info of (string option * string option)
-  | Buckets of string list
-  | Keys of string list
-
 type error = [ `Bad_payload | `Incomplete_payload | P.error ]
 
-type 'a cont = More of 'a | Done of 'a
+type 'a t = More of 'a | Done of 'a
 
 let run payload f =
   let open Result.Monad_infix in
@@ -23,62 +16,40 @@ let run payload f =
   P.run f s              >>= fun (r, _) ->
   Ok r
 
-let parse_error payload =
+let error payload =
   failwith "nyi"
 
-let parse_ping payload =
+let ping payload =
   match Bitstring.string_of_bitstring payload with
     | "" ->
-      Ok (Done Ping)
+      Ok (Done ())
     | _ ->
       Error `Bad_payload
 
-let parse_client_id payload =
+let client_id payload =
   let open Result.Monad_infix in
   run payload Pb_response.client_id >>= fun client_id ->
-  Ok (Done (Client_id client_id))
+  Ok (Done client_id)
 
-let parse_server_info payload =
+let server_info payload =
   let open Result.Monad_infix in
   run payload Pb_response.server_info >>= fun server_info ->
-  Ok (Done (Server_info server_info))
+  Ok (Done server_info)
 
-let parse_list_buckets payload =
+let list_buckets payload =
   let open Result.Monad_infix in
   run payload Pb_response.list_buckets >>= fun buckets ->
-  Ok (Done (Buckets buckets))
+  Ok (Done buckets)
 
-let parse_list_keys payload =
+let list_keys payload =
   let open Result.Monad_infix in
   run payload Pb_response.list_keys >>= function
     | (keys, false) ->
-      Ok (More (Keys keys))
+      Ok (More keys)
     | (keys, true) ->
-      Ok (Done (Keys keys))
+      Ok (Done keys)
 
-let message_code =
-  Int.Map.of_alist_exn
-    [ (0x00, parse_error)
-    ; (0x02, parse_ping)
-    ; (0x04, parse_client_id)
-    ; (0x08, parse_server_info)
-    ; (0x10, parse_list_buckets)
-    ; (0x12, parse_list_keys)
-    ]
-
-let find_mc mc =
-  match Int.Map.find message_code mc with
-    | Some prsr ->
-      Ok prsr
-    | None ->
-      Error `Wrong_type
-
-let parse_resp mc payload =
-  let open Result.Monad_infix in
-  find_mc mc >>= fun prsr ->
-  prsr payload
-
-let of_string s =
+let parse_mc s =
   let bits = Bitstring.bitstring_of_string s in
   let module Int32 = Old_int32 in
   let module Char = Old_char in
@@ -88,8 +59,7 @@ let of_string s =
     | { mc      : 8
       ; payload : -1 : bitstring
       } ->
-      parse_resp mc payload >>= fun resp ->
-      Ok resp
+      Ok (mc, payload)
     | { _ } ->
       Error `Incomplete_payload
 
