f6c47943905b6b0b9323c97c42272a705a9a50b2
orbitz <orbitz@gmail.com>
2013-06-29 17:23:48 +0200

    Modify get/put examples to support indices

--- examples/put.ml
+++ examples/put.ml
@@ -8,11 +8,24 @@ let hex_of_string =
   String.concat_map ~f:(fun c -> sprintf "%X" (Char.to_int c))
 
 let print_usermeta content =
-  let module P = Riakc.Robj.Pair in
+  let module U = Riakc.Robj.Usermeta in
   List.iter
-    ~f:(fun p ->
-      printf "USERMETA: %s = %s\n" (P.key p) (option_to_string (P.value p)))
+    ~f:(fun u ->
+      printf "USERMETA: %s = %s\n" (U.key u) (option_to_string (U.value u)))
     (Riakc.Robj.Content.usermeta content)
+
+let print_indices content =
+  let module I = Riakc.Robj.Index in
+  let index_value_to_string = function
+    | I.String s  -> "String " ^ s
+    | I.Integer i -> "Integer " ^ (Int.to_string i)
+    | I.Bad_int s -> "Bad_int " ^ s
+    | I.Unknown s -> "Unknown " ^ s
+  in
+  List.iter
+    ~f:(fun i ->
+      printf "INDEX: %s = %s\n" (I.key i) (index_value_to_string (I.value i)))
+    (Riakc.Robj.Content.indices content)
 
 let print_value content =
   let value = Riakc.Robj.Content.value content in
@@ -28,11 +41,47 @@ let print_contents =
       printf "CHARSET: %s\n" (option_to_string (C.charset content));
       printf "CONTENT_ENCODING: %s\n" (option_to_string (C.content_encoding content));
       print_usermeta content;
+      print_indices content;
       print_value content)
 
 let fail s =
   printf "%s\n" s;
   shutdown 1
+
+let parse_index s =
+  let module R = Riakc.Robj in
+  match String.lsplit2 ~on:':' s with
+    | Some ("bin", kv) -> begin
+      match String.lsplit2 ~on:':' kv with
+	| Some (k, v) ->
+	  R.Index.create ~k ~v:(R.Index.String v)
+	| None ->
+	  failwith ("Bad index: " ^ s)
+    end
+    | Some ("int", kv) -> begin
+      match String.lsplit2 ~on:':' kv with
+	| Some (k, v) ->
+	  R.Index.create ~k ~v:(R.Index.Integer (Int.of_string v))
+	| None ->
+	  failwith ("Bad index: " ^ s)
+    end
+    | _ ->
+      failwith ("Bad index: " ^ s)
+
+let rec add_2i r idx =
+  if idx < Array.length Sys.argv then
+    let module R = Riakc.Robj in
+    let content = List.hd_exn (R.contents r) in
+    let indices = R.Content.indices content in
+    add_2i
+      (R.set_content
+	 (R.Content.set_indices
+	    ((parse_index Sys.argv.(idx))::indices)
+	    content)
+	 r)
+      (idx + 1)
+  else
+    r
 
 let exec () =
   let host = Sys.argv.(1) in
@@ -46,6 +95,7 @@ let exec () =
     (fun c ->
       let module R = Riakc.Robj in
       let robj = R.set_content (R.Content.create v) (R.create []) in
+      let robj = add_2i robj 6 in
       Riakc.Conn.put c ~b ~k robj)
 
 let eval () =
